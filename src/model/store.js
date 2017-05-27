3da8ceebcf0525c72cbf6078dcd327e0c10d3f0d
JP DeVries <mail@devries.jp>
2017-05-27 15:57:16 +0200

    build

--- src/model/store.js
+++
@@ -1,8 +0,0 @@
-const redux = require('redux'),
-createStore = redux.createStore,
-applyMiddleware = redux.applyMiddleware,
-thunk = require('redux-thunk').default,
-reducers = require('./reducers'),
-store = createStore(reducers.CombinedReducers, applyMiddleware(thunk));
-
-module.exports = store;
